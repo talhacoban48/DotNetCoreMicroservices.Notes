062cd5d769578ffecba9107415d0977bc79c78e3
talhacoban48 <talha.coban@rastmobile.com>
2025-10-19 12:54:33 +0300

    changes applied

--- Mango/Mango.Services.CouponAPI/obj/Debug/net9.0/Mango.Services.CouponAPI.AssemblyInfo.cs
+++ Mango/Mango.Services.CouponAPI/obj/Debug/net9.0/Mango.Services.CouponAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62d31a88f434bf5e56aeb23d4efd15f6a5b8c0a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
