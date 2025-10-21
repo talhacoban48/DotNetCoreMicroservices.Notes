70a51b3df546c278202a31486fd15cbfa6a4a43d
talhacoban48 <talha.coban@rastmobile.com>
2025-10-21 16:48:38 +0300

    Product Api Base created

--- Mango/Mango.Services.CouponAPI/obj/Debug/net9.0/Mango.Services.CouponAPI.AssemblyInfo.cs
+++ Mango/Mango.Services.CouponAPI/obj/Debug/net9.0/Mango.Services.CouponAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d1fb3f2250569789e2e0b597b6cfbea6bbca553")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74e1f35ccace9d29b160562c6f8d87908695e618")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
