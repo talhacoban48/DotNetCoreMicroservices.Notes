267f6b9a0218d804e6ad8771a596541839ae756b
talhacoban48 <talha.coban@rastmobile.com>
2025-10-21 20:50:44 +0300

    theme change

--- Mango/Mango.Web/obj/Debug/net9.0/Mango.Web.AssemblyInfo.cs
+++ Mango/Mango.Web/obj/Debug/net9.0/Mango.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51c543418c4e424302e82cd571bbeb0a502ca363")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d831caf0f0993214194855ab8e635602d07970c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
